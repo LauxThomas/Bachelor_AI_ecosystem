cb58251019450d4bf83b784ae9e98492eb392977
_lauxiliary <32910534+LauxThomas@users.noreply.github.com>
2019-06-10 23:24:56 +0200

    added spawner, tweaked movement

---
+++ NeatEcosystem/Assets/OrganismSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrganismSpawner : MonoBehaviour
+{
+    public GameObject[] organismPrefabs;
+    public GameObject[] foodPrefabs;
+    public int borderrange = 100;
+
+    public float spawnTimer = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0)
+        {
+            spawnOrganism();
+            spawnTimer = Random.Range(2, 4);
+        }
+    }
+
+    private void spawnOrganism()
+    {
+        int randomSpawn = Random.Range(0, organismPrefabs.Length);
+        Vector3 position = new Vector3(Random.Range(-borderrange, borderrange), Random.Range(-borderrange, borderrange),
+            0);
+        Instantiate(organismPrefabs[randomSpawn], position, Quaternion.identity);
+    }
+}
--- NeatEcosystem/Assets/Scripts/MovementController.cs
+++ NeatEcosystem/Assets/Scripts/MovementController.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MovementController : MonoBehaviour
 {
     private float _portalTimer = 0.1f;
+    private float _targetTimer = 0f;
     public float speed = 3;
     private Transform _transform;
+    private Vector3 target = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +21,29 @@ public class MovementController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        _transform.position += Vector3.right * speed;
-        _transform.position += Vector3.up * speed;
+//        _transform.position += Vector3.right * speed;
+//        _transform.position += Vector3.up * speed;
+        target = calculateRandomPosition();
+        _transform.position =
+            Vector3.MoveTowards(_transform.position, target, speed);
         decrementPortalTimer();
     }
+
+    private Vector3 calculateRandomPosition()
+    {
+        if (_targetTimer > 0f && Vector3.Distance(transform.position, target) >= 25)
+        {
+            _targetTimer -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            _targetTimer = Random.Range(2, 6);
+            target = new Vector3(Random.Range(-1000, 1000), Random.Range(-1000, 1000), 0);
+        }
+
+        return target;
+    }
+
 
     private void decrementPortalTimer()
     {
@@ -34,23 +55,39 @@ public class MovementController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        String otherName = other.name.ToLower();
+        handleEnemyContact(other, otherName);
+
         if (_portalTimer <= 0)
         {
             _portalTimer = 0.1f;
             float x = _transform.position.x;
             float y = _transform.position.y;
             float z = _transform.position.z;
-            String otherName = other.name.ToLower();
 
             if (otherName.Contains("bottom") || otherName.Contains("upper"))
             {
                 _transform.position = new Vector3(x, -y, z);
-                Debug.Log("upperBottom");
             }
             else if (otherName.Contains("left") || otherName.Contains("right"))
             {
                 _transform.position = new Vector3(-x, y, z);
-                Debug.Log("leftRight");
+            }
+        }
+    }
+
+    private void handleEnemyContact(Collider2D other, string otherName)
+    {
+        if ((otherName.Contains("t01") && this.name.ToLower().Contains("t02")) ||
+            (otherName.Contains("t02") && this.name.ToLower().Contains("t01")))
+        {
+            if (Random.Range(0, 2) < 1)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                Destroy(other.gameObject);
             }
         }
     }
