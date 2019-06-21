c5d379261eb39a2aea4a3e42d6b21de98a8b0a79
_lauxiliary <32910534+LauxThomas@users.noreply.github.com>
2019-06-21 17:52:36 +0200

    added FoodStats to git... angain... quite important

--- Ecosystem2D/Assets/BibitProducer.cs
+++ Ecosystem2D/Assets/BibitProducer.cs
@@ -84,7 +84,7 @@ public class BibitProducer : MonoBehaviour
 //        }
 
         while (allBibits.Count < Camera.main.orthographicSize * 2)
-//        while (allBibits.Count < 1)
+//        while (allBibits.Count < 50)
         {
             spawnBibit();
             additionalBibitsProduced++;
