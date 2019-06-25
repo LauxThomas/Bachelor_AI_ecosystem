548f8dc2cc8f4a231b1b5da84f7f137898c591ad
_lauxiliary <32910534+LauxThomas@users.noreply.github.com>
2019-06-25 22:16:13 +0200

    reenabled rigidbodies and stuff

---
+++ Ecosystem2D/Assets/MapGenerator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    private float width;
+    private float height;
+    private Vector3 lu;
+    private Vector3 lo;
+    private Vector3 ru;
+    private Vector3 ro;
+
+    public int counter = 0;
+    public GameObject p1;
+    public GameObject p2;
+    private GameObject parent;
+
+    private void Start()
+    {
+        lu = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        lo = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0));
+        ru = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0));
+        ro = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+        width = Vector3.Distance(lu, ru);
+        height = Vector3.Distance(lu, lo);
+
+        float prefabsize = p1.GetComponent<BoxCollider2D>().size.x*p1.GetComponent<Transform>().localScale.x;
+        float prefabsizeHalf = prefabsize/2;
+        
+        for (float x = lu.x; x < ru.x; x+=prefabsize)
+        {
+            for (float y = lu.y; y < lo.y; y+=prefabsize)
+            {
+                GameObject spawner = Random.value < 0.5f ? p1 : p2;
+                Vector3 spawnPoint = new Vector3(x+prefabsizeHalf, y+prefabsizeHalf, 0);
+                GameObject go = Instantiate(spawner, spawnPoint, Quaternion.identity);
+                if (parent == null)
+                {
+                    parent = new GameObject("parent");
+                }
+
+                go.transform.parent = parent.transform;
+            }
+        }
+    }
+}
---
+++ Ecosystem2D/Assets/TileSet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileSet : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tilemap tilemap = GetComponent<Tilemap>();
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+        for (int x = 0; x < bounds.size.x; x++) {
+            for (int y = 0; y < bounds.size.y; y++) {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                if (tile != null) {
+                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                } else {
+                    Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                }
+            }
+        }  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
